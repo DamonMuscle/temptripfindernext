cd56412275d2b8ff4a81276e539602b2b42ea373
siyang.liao <siyang.liao@transfinder.com>
2018-06-21 16:55:51 +0800

    Ticket: #FT-248 SubTicket: # Component: Changes: 1.rename ScheduledReportReceiptDataModel to ReportReceiptDataModel

---
+++ TripfinderWeb/TripfinderWeb/Global/JavaScript/DataModel/ReportReceiptDataModel.js
@@ -0,0 +1,22 @@
+﻿(function()
+{
+	var namespace = window.createNamespace("TF.DataModel");
+	namespace.ReportReceiptDataModel = function(ReportReceiptEntity)
+	{
+		namespace.BaseDataModel.call(this, ReportReceiptEntity);
+	};
+
+	namespace.ReportReceiptDataModel.prototype = Object.create(namespace.BaseDataModel.prototype);
+
+	namespace.ReportReceiptDataModel.prototype.constructor = namespace.ReportReceiptDataModel;
+
+	namespace.ReportReceiptDataModel.prototype.mapping = [
+		{ from: "Id", default: 0, required: true },
+		{ from: "ScheduledReportId", default: 0 },
+		{ from: "SelectedUserId", default: 0 },
+		{ from: "RecipientType", default: 0 },
+		{ from: "UserName", default: "" },
+		{ from: "EmailAddress", default: "" }
+	];
+
+})();
