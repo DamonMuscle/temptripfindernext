1bb2e09ca913a1681634acd39ca656cd912579c8
wei.xiao <wei.xiao@transfinder.com>
2018-07-09 15:47:31 +0800

    Ticket: #no ticket SubTicket: # Component: Changes: 1. Fix a bug of detail view.

--- TripfinderWeb/TripfinderWeb/Global/JavaScript/DataModel/DetailScreenLayoutDataModel.js
+++ TripfinderWeb/TripfinderWeb/Global/JavaScript/DataModel/DetailScreenLayoutDataModel.js
@@ -14,7 +14,7 @@
 		{ from: "Id", default: 0 },
 		{ from: "Name", default: "" },
 		{ from: "Table", default: "" },
-		{ from: "Layout", default: '{"items":[],"width":4}' },
+		{ from: "Layout", default: '{"width":4,"sliderFontRate":0.5,"items":[]}' },
 		{ from: "Comments", default: "" },
 		{ from: "SubTitle", default: "" }
 	];
