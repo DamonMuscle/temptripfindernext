9facff7a4393aa8c2560deef731fdcdc2487e85b
wei.xiao <wei.xiao@transfinder.com>
2018-07-10 14:44:59 +0800

    Ticket: #FT-297 SubTicket: # Component: Changes: 1. remove useless functionalities.

--- TripfinderWeb/TripfinderWeb/Global/JavaScript/ViewModel/Pages/SchedulerPage.js
+++ TripfinderWeb/TripfinderWeb/Global/JavaScript/ViewModel/Pages/SchedulerPage.js
@@ -362,7 +362,8 @@
 							}
 						}
 					});
-				}
+				},
+				footer: false
 			}).data("kendoScheduler");
 			self.setFilterOpitons();
 
@@ -372,31 +373,22 @@
 
 	SchedulerPage.prototype.getOriginalDataSource = function(type)
 	{
-		var self = this, url, params;
+		var self = this, url, params = {
+			"sortItems": [{ "Name": "PublicId" }, { "Name": "Id", "isAscending": "asc" }],
+			"idFilter": { "IncludeOnly": null, "ExcludeAny": [] },
+			"filterSet": null,
+			"filterClause": "",
+			"isQuickSearch": false,
+			"fields": ["PublicId", "FieldTripStageName", "Name", "ReturnDate", "DepartDate", "DepartTime", "ReturnTime", "Id", "FieldTripStageId", "DepartDateTime"]
+		};
 
 		switch (type)
 		{
 			case "fieldtrips":
 				url = pathCombine(tf.api.apiPrefix(), "search", "fieldtrip", "permission");
-				params = {
-					"sortItems": [{ "Name": "PublicId" }, { "Name": "Id", "isAscending": "asc" }],
-					"idFilter": { "IncludeOnly": null, "ExcludeAny": [] },
-					"filterSet": null,
-					"filterClause": "",
-					"isQuickSearch": false,
-					"fields": ["PublicId", "FieldTripStageName", "Name", "ReturnDate", "DepartDate", "DepartTime", "ReturnTime", "Id", "FieldTripStageId", "DepartDateTime"]
-				};
 				break;
 			case "myrequests":
 				url = pathCombine(tf.api.apiPrefix(), "search", "fieldtrip", "submitted");
-				params = {
-					"sortItems": [{ "Name": "PublicId" }, { "Name": "Id", "isAscending": "asc" }],
-					"idFilter": { "IncludeOnly": null, "ExcludeAny": [] },
-					"filterSet": null,
-					"filterClause": "",
-					"isQuickSearch": false,
-					"fields": ["PublicId", "FieldTripStageName", "Name", "ReturnDate", "DepartDate", "DepartTime", "ReturnTime", "Id", "FieldTripStageId", "DepartDateTime"]
-				};
 				break;
 			default:
 				break;
