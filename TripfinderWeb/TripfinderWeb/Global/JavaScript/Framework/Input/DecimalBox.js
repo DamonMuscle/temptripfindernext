e75a949ad0fb8f92480b2b2ded7ff36c5716864c
changcheng.jiang <changcheng.jiang@transfinder.com>
2018-11-01 09:21:56 +0800

    Ticket: #FT-893 SubTicket: # Component: Changes: 1.Fix bug: Value in input box cannot be deleted when edit a trip cost in Firefox

--- TripfinderWeb/TripfinderWeb/Global/JavaScript/Framework/Input/DecimalBox.js
+++ TripfinderWeb/TripfinderWeb/Global/JavaScript/Framework/Input/DecimalBox.js
@@ -10,6 +10,11 @@
 		this.getElement().on("keypress keyup blur", function(event)
 		{
 			var keyCode = event.which || event.keyCode || 0;
+			//Fix bug FT-893: in FireFox, keypress event is fired when press backspace key. In Chrome, keypress event is not fired.
+			if (event.type == "keypress" && keyCode == 8)
+			{
+				return;
+			}
 
 			if ((keyCode != 46 || $(this).val().indexOf('.') !== -1) && (keyCode != 45 || ($(this).val().indexOf('-') !== -1 ? this.selectionStart > 0 || this.selectionEnd === 0 : false) || this.selectionStart > 0) && (keyCode < 48 || keyCode > 57) && keyCode !== 37 && keyCode !== 39 && keyCode !== 9)
 			{
