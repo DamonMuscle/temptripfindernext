acc4d17f54f757e66e12124d6d621e6c4f1f5a80
wei.xiao <wei.xiao@transfinder.com>
2018-11-07 14:13:25 +0800

    Ticket: #no ticket SubTicket: # Component: Changes: 1. Fix a bug that when update the columns in the list move, js error occurs

--- TripfinderWeb/TripfinderWeb/Global/JavaScript/Workspace/GridDefinition/FieldTripGridDefinition.js
+++ TripfinderWeb/TripfinderWeb/Global/JavaScript/Workspace/GridDefinition/FieldTripGridDefinition.js
@@ -29,7 +29,7 @@
 					DisplayName: "Status",
 					Width: '250px',
 					type: "string",
-					template: "<div style='height:15px;width:15px;margin-right:.5em;border:1px solid rgb(213, 213, 213);background-color:#: tf.fieldTripGridDefinition.gridDefinition().stageFormatter(data.FieldTripStageId)#;float:left'></div><span>#:FieldTripStageName#</span>"
+					template: "<div style='height:15px;width:15px;margin-right:.5em;border:1px solid rgb(213, 213, 213);background-color:#: tf.fieldTripGridDefinition.gridDefinition().stageFormatter(data.FieldTripStageId)#;float:left'></div><span>#:FieldTripStageName||''#</span>"
 				},
 				{
 					FieldName: "Name",
