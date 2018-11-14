bfdc7cf7e5caeccead989da193bfd6b42c908f82
james.zhao <james.zhao@transfinder.com>
2018-11-14 14:00:29 +0800

    Ticket: #FT-1038 SubTicket: # Component:As a level 5 User I should have a Pending Approvals Page Instead of Submitted Requests Changes: 1. Change code back to Cait's version.

--- TripfinderWeb/TripfinderWeb/Global/JavaScript/Framework/AuthManager.js
+++ TripfinderWeb/TripfinderWeb/Global/JavaScript/Framework/AuthManager.js
@@ -235,7 +235,7 @@
 		if (this.isAdmin || this.isFieldTripAdmin) return false;
 
 		var securedItems = this.authorizationTree.securedItems;
-		return !["level2Administrator", "level3Administrator", "level4Administrator", "transportationAdministrator"].some(function(item)
+		return !["level2Administrator", "level3Administrator", "level4Administrator"].some(function(item)
 		{
 			var securedItem = securedItems[item];
 			return securedItem && securedItem.length;
