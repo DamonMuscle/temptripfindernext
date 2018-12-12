1914a0ec83d18fde536830b727f620c1d54b4109
wei.xiao <wei.xiao@transfinder.com>
2018-12-12 11:21:25 +0800

    Ticket: #ENT-790 SubTicket: # Component: Changes: 1. Update the tabindex of the login page.

--- TripfinderWeb/TripfinderWeb/Global/JavaScript/Framework/ModalManager.js
+++ TripfinderWeb/TripfinderWeb/Global/JavaScript/Framework/ModalManager.js
@@ -103,7 +103,7 @@
 			}
 
 			//these input is not focused when user click tab key in the keyboard
-			var $inputs = $modal.find(".modal-body").find("input:button,input:image,input:file,input.notinput-required-message,button,input[data-tf-input-type='Select']");
+			var $inputs = $modal.find(".modal-body").find("input:button,input:image,input:file,input.notinput-required-message,button:not(.btn-login),input[data-tf-input-type='Select']");
 			$inputs.attr("tabIndex", -1);
 		});
 
