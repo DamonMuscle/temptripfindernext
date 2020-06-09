abe964db02b17040b0ae8eb23de28d108ded215c
lianbo.lu <lianbo.lu@transfinder.com>
2020-06-09 14:59:39 +0800

    Issue: #FT-1639 Component: Changes: 1.No admin users cannot see all the products assigned for their role

--- TripfinderWeb/TripfinderWeb/Global/JavaScript/Framework/PageManager.js
+++ TripfinderWeb/TripfinderWeb/Global/JavaScript/Framework/PageManager.js
@@ -65,7 +65,9 @@
 			{
 				var productName = prod.toLowerCase();
 				return self.availableApplications.hasOwnProperty(productName);
-			}).map(v => v.toLowerCase());
+			}).map(function(v) {
+				return v.toLowerCase();
+			});
 			                
 			if (tf.authManager.authorizationInfo.isAdmin)
 			{
