bbc175f9ec79142f045559cb44f9ea1d081d8061
Gerrard.Li <Gerrard.Li@transfinder.com>
2020-06-29 16:22:17 +0800

    Issue: #FT-1694 SubTicket: # Component: Changes: 1. Change style for mobile Device

--- TripfinderWeb/TripfinderWeb/Global/JavaScript/ViewModel/Workspace/DetailView/ManageAssociation/BasicQuickAddModalViewModel.js
+++ TripfinderWeb/TripfinderWeb/Global/JavaScript/ViewModel/Workspace/DetailView/ManageAssociation/BasicQuickAddModalViewModel.js
@@ -1,65 +1,56 @@
-(function()
-{
-	createNamespace("TF.DetailView").BasicQuickAddModalViewModel = BasicQuickAddModalViewModel;
+(function() {
+    createNamespace("TF.DetailView").BasicQuickAddModalViewModel = BasicQuickAddModalViewModel;
 
-	function BasicQuickAddModalViewModel(options)
-	{
-		var self = this,
-			dataType = options.dataType,
-			modeName = !options.recordId ? "Add" : "Edit",
-			typeName = tf.dataTypeHelper.getFormalDataTypeName(dataType),
-			title = String.format("{0} {1}", modeName, typeName),
-			viewModel = new TF.DetailView.BasicQuickAddViewModel(options);
+    function BasicQuickAddModalViewModel(options) {
+        var self = this,
+            dataType = options.dataType,
+            modeName = !options.recordId ? "Add" : "Edit",
+            typeName = tf.dataTypeHelper.getFormalDataTypeName(dataType),
+            title = String.format("{0} {1}", modeName, typeName),
+            viewModel = new TF.DetailView.BasicQuickAddViewModel(options);
 
-		TF.Modal.BaseModalViewModel.call(self);
+        TF.Modal.BaseModalViewModel.call(self);
 
-		self.sizeCss = "modal-dialog-xl";
-		self.modalClass = 'quick-add enable-tab';
-		self.data(viewModel);
-		self.title(title);
-		self.contentTemplate("Workspace/detailview/ManageAssociation/BasicQuickAdd");
-		self.buttonTemplate("modal/positivenegative");
-		self.obPositiveButtonLabel("Save & Close");
-	};
+        self.sizeCss = TF.isMobileDevice ? "modal-dialog-lg" : "modal-dialog-xl";
+        self.modalClass = 'quick-add enable-tab';
+        self.data(viewModel);
+        self.title(title);
+        self.contentTemplate("Workspace/detailview/ManageAssociation/BasicQuickAdd");
+        self.buttonTemplate("modal/positivenegative");
+        self.obPositiveButtonLabel("Save & Close");
+    };
 
-	BasicQuickAddModalViewModel.prototype = Object.create(TF.Modal.BaseModalViewModel.prototype);
-	BasicQuickAddModalViewModel.prototype.constructor = BasicQuickAddModalViewModel;
+    BasicQuickAddModalViewModel.prototype = Object.create(TF.Modal.BaseModalViewModel.prototype);
+    BasicQuickAddModalViewModel.prototype.constructor = BasicQuickAddModalViewModel;
 
-	/**
-	 * React when the positive button is clicked.
-	 * @return {void}
-	 */
-	BasicQuickAddModalViewModel.prototype.positiveClick = function()
-	{
-		var self = this;
-		self.data().save().then(function(result)
-		{
-			if (result) 
-			{
-				self.positiveClose(result);
-			}
-			else if (result !== false)
-			{
-				self.negativeClose();
-			}
-		});
-	};
+    /**
+     * React when the positive button is clicked.
+     * @return {void}
+     */
+    BasicQuickAddModalViewModel.prototype.positiveClick = function() {
+        var self = this;
+        self.data().save().then(function(result) {
+            if (result) {
+                self.positiveClose(result);
+            } else if (result !== false) {
+                self.negativeClose();
+            }
+        });
+    };
 
-	/**
-	 * React when the negative button is clicked.
-	 * @return {void}
-	 */
-	BasicQuickAddModalViewModel.prototype.negativeClick = function()
-	{
-		this.negativeClose(false);
-	};
+    /**
+     * React when the negative button is clicked.
+     * @return {void}
+     */
+    BasicQuickAddModalViewModel.prototype.negativeClick = function() {
+        this.negativeClose(false);
+    };
 
-	/**
-	 * Dispose
-	 * @return {void}
-	 */
-	BasicQuickAddModalViewModel.prototype.dispose = function()
-	{
-		this.data().dispose();
-	};
+    /**
+     * Dispose
+     * @return {void}
+     */
+    BasicQuickAddModalViewModel.prototype.dispose = function() {
+        this.data().dispose();
+    };
 })();
