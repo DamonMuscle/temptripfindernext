25cd0cf699da1fda1f3be2af493a0d7b3176d9dd
Jiaqi.Cai <jiaqi.cai@transfinder.com>
2020-08-04 15:54:22 +0800

    Issue: #FT-1843 Component: Changes: Add "Purchase Order" data block in Tripfinder's detail view.

--- TripfinderWeb/TripfinderWeb/Global/JavaScript/ViewModel/DetailView/DataPointsJSON.js
+++ TripfinderWeb/TripfinderWeb/Global/JavaScript/ViewModel/DetailView/DataPointsJSON.js
@@ -1556,6 +1556,16 @@ var today = (new Date()).toDateString(),
 					"type": "Number",
 					"format": "Money",
 					"defaultValue": "0.00"
+				},
+				{
+					"field": "PurchaseOrder",
+					"title": "Purchase Order",
+					"type": "String",
+					"defaultValue": "",
+					"editType": {
+						"format": "String",
+						"maxLength": 50
+					}
 				}
 			],
 			"User Defined": [],
