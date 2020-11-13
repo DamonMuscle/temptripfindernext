e85fdfae81c5b3f3dfbdf413ed2dabb4e70f9fb0
qiaohong.feng <qiaohong.feng@transfinder.com>
2020-11-13 08:24:47 +0800

    Issue: # FT-1884 Component: Changes: 1. Implement the note message when opening the Tripfinder in the IE broswsers.

--- TripfinderWeb/TripfinderWeb/Global/JavaScript/Startup.js
+++ TripfinderWeb/TripfinderWeb/Global/JavaScript/Startup.js
@@ -264,6 +264,12 @@
 	function Startup() { }
 	Startup.prototype.start = function()
 	{
+		if(TF.isIE)
+		{
+			window.location.href = 'ie.html';
+			return;
+		}
+
 		var self = this;
 		self.libraryInitialization().then(function()
 		{
