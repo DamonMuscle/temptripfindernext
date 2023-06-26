c4372950517747de4da9beeafc813cd94d14eb21
chenjie.deng <chenjie.deng@transfinder.com>
2023-06-26 10:15:32 +0800

    Issue: #FT-3274 Component: Changes: 1.Fix Bug-Callout is not shown after some operations

--- TripfinderWeb/TripfinderWeb/Global/JavaScript/Framework/Map/MapTools/RoutingMapTool.js
+++ TripfinderWeb/TripfinderWeb/Global/JavaScript/Framework/Map/MapTools/RoutingMapTool.js
@@ -83,6 +83,7 @@
 		if (toolName !== "manuallyPinTool" && previousToolName === "manuallyPinTool")
 		{
 			this.manuallyPinTool && this.manuallyPinTool.stopPin();
+			this._manuallyPinActive = false;
 		}
 		if (toolName !== "drawGeoregionBoundaryTool" && previousToolName === "drawGeoregionBoundaryTool")
 		{
