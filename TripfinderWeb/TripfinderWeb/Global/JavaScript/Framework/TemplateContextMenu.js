853bcd92b94629e4bb64462800eaa1854bd0b4e9
chenjie.deng <chenjie.deng@transfinder.com>
2023-06-28 16:06:17 +0800

    Issue: #FT-3082 Component: Changes: 1.Fix Bug-Details View-There is no response when clicking date picker - Consistent with the plus code

--- TripfinderWeb/TripfinderWeb/Global/JavaScript/Framework/TemplateContextMenu.js
+++ TripfinderWeb/TripfinderWeb/Global/JavaScript/Framework/TemplateContextMenu.js
@@ -4,31 +4,42 @@
 	var menusNeedHideLeftBarTopBorder = [
 		'workspace/grid/filtercontextmenu',
 		'workspace/grid/layoutcontextmenu',
+		'workspace/grid/overflowmenu',
 		'workspace/grid/newgridwithselectedrecordsmenu',
 		'workspace/grid/editkendocolumnformobile'
 	];
 
 	namespace.TemplateContextMenu = TemplateContextMenu;
 
-	function TemplateContextMenu(templateName, menuViewModel)
+	function TemplateContextMenu(templateName, menuViewModel, done)
 	{
 		namespace.BaseContextMenu.apply(arguments);
-		if (!menuViewModel instanceof TF.ContextMenu.BaseGeneralMenuViewModel)
-		{
-			throw "parameter should be a subclass of TF.ContextMenu.BaseGeneralMenuViewModel";
-		}
 
 		this.menuViewModel = menuViewModel;
+		this.done = done;
 
-		this.afterRender = function()
+		this.afterRender = function(elements, viewModel)
 		{
 			this.loopInit(this.$menuContainer);
 			this.setMenuMaxHeight(this.$menuContainer);
 			this.setMenuPosition(this.$menuContainer, this.target);
 
+			var mobileHelper = TF.ContextMenuMobileHelper;
+			if (TF.isPhoneDevice && !tf.isViewfinder)
+			{
+				var specialMenuClass = mobileHelper.getSpecialMenuClass(this.$menuContainer);
+				if (specialMenuClass)
+				{
+					mobileHelper.setMenuHeightForPhoneDevice(this.$menuContainer, specialMenuClass);
+					mobileHelper.setMenuLeftForPhoneDevice(this.$menuContainer);
+					mobileHelper.setMenuTopForPhoneDevice(this.$menuContainer);
+					this._$container.off("mouseout");
+					this._$container.off("mouseover");
+				}
+			}
 			if (this.menuViewModel.afterRender)
 			{
-				this.menuViewModel.afterRender();
+				this.menuViewModel.afterRender(elements, viewModel);
 			}
 		}.bind(this);
 
@@ -38,9 +49,17 @@
 			var $container = this.createContainer($wrapper, target);
 			this.$container = $container;
 			this.templateName = 'workspace/documenttabmenu';
-			var $templateWrapper = $('<div data-bind="template:{name:\''
-				+ templateName +
-				'\',afterRender:afterRender,data:menuViewModel}" style="display:none"></div>');
+			var $templateWrapper = $('<div data-bind="template:{name:\'' + templateName + '\''
+				+ ',afterRender:afterRender,data:menuViewModel}" style="display:none;background-color:#FFF"></div>');
+
+			if (TF.isPhoneDevice)
+			{
+				$templateWrapper.addClass('is-phone-device');
+			}
+			else if (TF.isMobileDevice)
+			{
+				$templateWrapper.addClass('is-mobile-device');
+			}
 			this.$menuContainer = $templateWrapper;
 
 			if (TF.isPhoneDevice && Enumerable.From(menusNeedHideLeftBarTopBorder)
@@ -60,7 +79,43 @@
 
 	TemplateContextMenu.prototype.loopInit = function(el)
 	{
-		var arrayOfMenuItem = [];
+		function showMenu($menu, $parent)
+		{
+			clearTimeout($menu.timer);
+			var left = $parent.position().left + $parent.width();
+			if (TF.isPhoneDevice || TF.isIOS)
+			{
+				left = left - $menu.outerWidth();
+			}
+			else
+			{
+				if ($parent.offset().left + left + $menu.outerWidth() > $(window).width())
+				{
+					left = 0 - $menu.outerWidth();
+				}
+			}
+			var top = $parent.position().top;
+			var outerHeight = $menu.outerHeight();
+			if ($parent.offset().top + outerHeight > $(window).height())
+			{
+				top = top + $parent.height() - outerHeight;
+			}
+			if (TF.isMobileDevice)
+			{
+				$menu.css({ position: "absolute", left: left + "px", top: top + "px", "z-index": 1 });
+			}
+			else
+			{
+				$menu.css({ position: "absolute", left: left + "px", top: top + "px" });
+			}
+			$menu.show();
+			if ($menu.offset().top < 0)
+			{
+				$menu.offset({ top: 0, left: $menu.offset().left });
+			}
+		}
+
+		var arrayOfMenuItem = [], menuItem;
 		$.merge(arrayOfMenuItem, $(el).find(".menu-item"));
 		while (menuItem = arrayOfMenuItem.shift())
 		{
@@ -76,27 +131,21 @@
 						$menu.hide();
 
 						$.merge(arrayOfMenuItem, $menu.find(".menu-item"));
-						$menuItem.on("mouseover", function(e)
+						if (TF.isMobileDevice)
 						{
-							clearTimeout($menu.timer);
-							var left = $parent.position().left + $parent.width();
-							if ($parent.offset().left + left + $menu.outerWidth() > $(window).width())
+							$menuItem.on("click", function(e)
 							{
-								left = 0 - $menu.outerWidth();
-							}
-							var top = $parent.position().top;
-							var outerHeight = $menu.outerHeight();
-							if ($parent.offset().top + outerHeight > $(window).height())
+								showMenu($menu, $parent)
+							});
+						}
+						else
+						{
+							$menuItem.on("mouseover", function(e)
 							{
-								top = top + $parent.height() - outerHeight;
-							}
-							$menu.css({ position: "absolute", left: left + "px", top: top + "px" });
-							$menu.show();
-							if ($menu.offset().top < 0)
-							{
-								$menu.offset({ top: 0, left: $menu.offset().left });
-							}
-						});
+								showMenu($menu, $parent)
+							});
+						}
+
 						$menuItem.on("mouseleave", function(e)
 						{
 							clearTimeout($menu.timer);
@@ -116,9 +165,16 @@
 		}
 	};
 
+	function isSplitPanelOpenedOnMobileDevicePortraitMode()
+	{
+		return TF.isMobileDevice &&
+			TF.isPortrait() &&
+			$('.right-panel').height() > 0;
+	}
+
 	TemplateContextMenu.prototype.setMenuMaxHeight = function(el)
 	{
-		var arrayOfContextMenu = [];
+		var arrayOfContextMenu = [], contextMenu;
 		$.merge(arrayOfContextMenu, $(el).find(".tf-contextmenu"));
 		while (contextMenu = arrayOfContextMenu.shift())
 		{
@@ -126,6 +182,12 @@
 			{
 				var $contextMenu = $(contextMenu);
 				var dynamicMaxHeight = $(window).height() - 300;
+				if (isSplitPanelOpenedOnMobileDevicePortraitMode())
+				{
+					var iconRowHeight = $('.iconrow').outerHeight();
+					dynamicMaxHeight = Math.min(dynamicMaxHeight, $(window).height() / 2 - iconRowHeight);
+				}
+
 				if (TF.isMobileDevice && $(window).height() <= 500 && $(window).height() >= 320)
 				{
 					dynamicMaxHeight = 238;
@@ -156,6 +218,10 @@
 	{
 		namespace.BaseContextMenu.prototype.dispose.call(this);
 		ko.cleanNode(this.$menuContainer[0]);
+		if (this.done)
+		{
+			this.done();
+		}
 		if (this.menuViewModel && this.menuViewModel.dispose)
 		{
 			this.menuViewModel.dispose();
@@ -172,4 +238,71 @@
 	{
 
 	}
+})();
+
+(function()
+{
+	function ContextMenuMobileHelper()
+	{
+
+	}
+
+	createNamespace("TF").ContextMenuMobileHelper = ContextMenuMobileHelper;
+
+	ContextMenuMobileHelper.setMenuHeightForPhoneDevice = function(el, specialMenuClass)
+	{
+		var $container = $(el);
+		var $contextmenuWrap = $container.find(specialMenuClass);
+		if ($contextmenuWrap.length)
+		{
+			var $contextmenu = $contextmenuWrap.find('.tf-contextmenu'),
+				originalContextMenuHeight = $contextmenu.height();
+
+			var gridHeight = $('.grid-wrap').height();
+			var contextMenuHeight = gridHeight > originalContextMenuHeight ? originalContextMenuHeight : gridHeight - 5;
+
+			$container.height(contextMenuHeight);
+			$contextmenuWrap.height(contextMenuHeight);
+			$contextmenu.css('height', contextMenuHeight + 'px');
+			$contextmenu.css('max-height', contextMenuHeight + 'px');
+		}
+	};
+
+	ContextMenuMobileHelper.setMenuLeftForPhoneDevice = function(el)
+	{
+		var $container = $(el);
+		var $containerWrap = $container.parent();
+		$container.css('left', '0');
+		$containerWrap.css('left', '0');
+	};
+
+	ContextMenuMobileHelper.setMenuTopForPhoneDevice = function(el)
+	{
+		var $container = $(el);
+		$container.css('top', '0');
+		var baseTop = $('.grid-panel').offset().top;
+
+		var $containerWrap = $container.parent();
+
+		var iconrowIsGridPanelChild = $('.grid-panel').find('.iconrow').length;
+		if (iconrowIsGridPanelChild)
+		{
+			var iconRowHeight = $('.iconrow').outerHeight();
+			$containerWrap.css('top', baseTop + iconRowHeight + 'px');
+		}
+		else
+		{
+			var hackOffsetTop = -1; // move up menu to concat tab button and menu
+			$containerWrap.css('top', baseTop + hackOffsetTop + 'px');
+		}
+	};
+
+	ContextMenuMobileHelper.getSpecialMenuClass = function(el)
+	{
+		var $contextmenuWrap = $(el).find('.phone-wrap');
+		if ($contextmenuWrap.length)
+			return '.phone-wrap';
+
+		return '';
+	};
 })();
