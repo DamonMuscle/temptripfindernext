942d6ad0ea4b330a9c0274a83e35d48eaedd7d38
damon.huang <damon.huang@transfinder.com>
2023-07-06 15:32:23 +0800

    Issue: #FT-3289 SubTicket: #FT-3384 Component: Changes: 1. refine columns' width

--- TripfinderWeb/TripfinderWeb/Global/JavaScript/ViewModel/Workspace/RoutingMap/RoutingMapPanel/RoutingPalette/ViewFieldTripViewModel.js
+++ TripfinderWeb/TripfinderWeb/Global/JavaScript/ViewModel/Workspace/RoutingMap/RoutingMapPanel/RoutingPalette/ViewFieldTripViewModel.js
@@ -18,14 +18,14 @@
 			{
 				FieldName: "PublicId",
 				DisplayName: "ID",
-				Width: '150px',
+				Width: '80px',
 				type: "string",
 				isSortItem: true
 			},
 			{
 				FieldName: "FieldTripStageName",
 				DisplayName: "Status",
-				Width: '250px',
+				Width: '200px',
 				type: "string",
 				template: `<div style='height:15px;width:15px;margin-right:.5em;border:1px solid rgb(213, 213, 213);
 				background-color:#: tf.fieldTripGridDefinition.gridDefinition().stageFormatter(data.FieldTripStageId)#;float:left'></div><span>#:FieldTripStageName#</span>`
